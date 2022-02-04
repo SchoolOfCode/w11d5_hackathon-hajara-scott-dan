750881df0b743d84fba345a9057c3bd86ca82b4a
D20CM <scottbrown500@hotmail.com>
2022-02-04 12:32:13 +0000

    add play button and word generator

---
+++ notes.md
@@ -0,0 +1,11 @@
+# not-hangman WORJIE game
+
+- app
+  - emoji
+  - letter input (1 letter max)
+  - submit button
+  - letter area
+    - letter displays
+  - hint display
+  - hint button
+  - play button
--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import logo from "./soc-logo.svg";
 import "./App.css";
+import Play from "./Components/Play/Play.js";
+import words from "./words-data/words.js";
+import { useState } from "react";
 
 function App() {
+  const [wordList, setWordList] = useState(words);
+  const [currentWord, setCurrentWord] = useState("");
+
+  function randomWord(wordList) {
+    let min = Math.ceil(0);
+    let max = Math.floor(5);
+    let random = Math.floor(Math.random() * (max - min) + min);
+
+    setCurrentWord(wordList[random]);
+    console.log("The word is: " + currentWord);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hackathon Friday</p>
-      </header>
+      {/* <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p className="App-logo">Worjie!</p>
+      </header> */}
+
+      <h1>Worjie!</h1>
+      <Play
+        clickHandler={() => {
+          randomWord(wordList);
+        }}
+      />
     </div>
   );
 }
